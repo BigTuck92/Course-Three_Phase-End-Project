2ea2c38bef0a1b71f9a675b7d3f72509eb4c47d8
Nathan Tucker <nathan.tucker92@gmail.com>
2024-09-14 10:03:45 -0500

    First Commit

---
+++ mycabbie/src/test/java/com/coursethreephaseend/mycabbie/MycabbieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursethreephaseend.mycabbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycabbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
