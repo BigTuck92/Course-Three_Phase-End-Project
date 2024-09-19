47a406135a36fa097ee5bd13504dbec4f3f6d702
Nathan Tucker <nathan.tucker92@gmail.com>
2024-09-19 18:27:20 -0500

    Created new Front End Project

--- MyCabbieFrontEnd/src/app/app.routes.ts
+++ MyCabbieFrontEnd/src/app/app.routes.ts
@@ -1,22 +1,3 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { CabBookingComponent } from './cab-booking/cab-booking.component';
-import { CabFareComponent } from './cab-fare/cab-fare.component';
 
-export const routes: Routes = [
-    {
-        path: "",
-        component: AppComponent,
-        title: "Home Page"
-    },
-    {
-        path: "cab-booking",
-        component: CabBookingComponent,
-        title: "Cab Booking"
-    },
-    {
-        path: "cab-fare",
-        component: CabFareComponent,
-        title: "Cab Fare"
-    }
-];
+export const routes: Routes = [];
