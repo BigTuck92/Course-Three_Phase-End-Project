2ffa6d6d3ebdd142f6b77008f40ec3083dd678a8
Nathan Tucker <nathan.tucker92@gmail.com>
2024-09-19 21:20:00 -0500

    Updated ViewController

--- BookingMS/src/main/java/Project/MyCabbie/BookingMS/controller/ViewController.java
+++ BookingMS/src/main/java/Project/MyCabbie/BookingMS/controller/ViewController.java
@@ -2,16 +2,33 @@ package Project.MyCabbie.BookingMS.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import Project.MyCabbie.BookingMS.model.Cab;
 
+
+@Controller
 public class ViewController {
 
 	// Fields
 	private Cab sedan = new Cab(null, "Sedan", 10);
 	private Cab suv = new Cab(null, "SUV", 15);
 	private Cab luxury = new Cab(null, "Luxury", 20);
-	
-	// Populate the Array List
 	private ArrayList<Cab> cabs = new ArrayList<Cab>();
+	
+	@GetMapping("")
+	public String showCabs(Model model) {
+		// Populate ArrayList
+		cabs.add(sedan);
+		cabs.add(suv);
+		cabs.add(luxury);
+		
+		// Populate Model
+		model.addAttribute("Cab", cabs);
+		
+		return null;
+	}
 	
 }
