e88cc398bd08a6b0fd8d111f52b1f8d5b5a8476c
Nathan Tucker <nathan.tucker92@gmail.com>
2024-09-19 19:04:17 -0500

    Initialized Booking MicroService

---
+++ BookingMS/src/test/java/Project/MyCabbie/BookingMS/BookingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.MyCabbie.BookingMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
