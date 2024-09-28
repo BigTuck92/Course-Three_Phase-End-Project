65f732f7b306df9c3a464cec6bcd536b18d442f8
Nathan Tucker <nathan.tucker92@gmail.com>
2024-09-28 12:19:26 -0500

    Added alert message to browser and form reset methods

--- MyCabbieFrontEnd/src/app/app.component.ts
+++ MyCabbieFrontEnd/src/app/app.component.ts
@@ -25,15 +25,36 @@ export class AppComponent {
 
   constructor(private bookingService: BookingServiceService) {}
 
+  calculateTotalFare(): number {
+    return this.booking.cabRate * this.booking.distance;
+  }
+
   submitBooking() {
+    this.booking.totalFare = this.calculateTotalFare();
+
     this.bookingService.createBooking(this.booking).subscribe({
       next: (response) => {
         console.log('Booking created:', response);
         // Optionally reset the form or show a success message
+        this.showSuccessMessage(`Booking Created Successfully, projected fare is $${this.booking.totalFare}`);
+        this.resetForm();
       },
       error: (error) => {
         console.error('Error creating booking:', error);
       }
     });
+  }
+
+  showSuccessMessage(message: any){
+    alert(message);
+  }
+
+  resetForm() {
+    this.booking = {
+      bookingId: 0, // Or any default value
+      cabRate: 0,
+      distance: 0,
+      totalFare: 0,
+    };
   }
 }
